1c0553f6607b6c1244a8e76ec8ce5c222c9814d6
Alois Cochard <alois.cochard@gmail.com>
2013-10-27 18:26:19 +0000

    Setup publishing for TimeOut repository.

--- project/Build.scala
+++ project/Build.scala
@@ -4,17 +4,25 @@ import sbtassembly.Plugin._
 import AssemblyKeys._
 
 object DrScala extends Build {
-  lazy val sharedSettings = Defaults.defaultSettings ++ Seq(
+  lazy val sharedSettings = Defaults.defaultSettings ++ publishSettings ++ Seq(
     scalaVersion := "2.10.3",
     organization := "com.timeout",
     version := "0.1.0-SNAPSHOT",
     description := "A doctor for your code"
   )
 
+  lazy val publishSettings = Seq(
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+    publishTo := Some("TimeOut Snapshots Nexus" at "http://nexus.repo.timeout.com/nexus/content/repositories/releases"),
+    publishMavenStyle := true,
+    publishArtifact := true
+  )
+
   lazy val root = Project(
     id = "root",
     base = file("."),
-    aggregate = Seq(common, plugin)
+    aggregate = Seq(common, plugin),
+    settings = sharedSettings ++ Seq(publishArtifact := false)
   )
 
   lazy val common = Project(
@@ -28,7 +36,8 @@ object DrScala extends Build {
   lazy val plugin = Project(
     id   = "drscala-plugin",
     base = file("plugin"),
-    settings = sharedSettings ++ assemblySettings) settings (
+    settings = sharedSettings ++ assemblySettings ++ addArtifact(artifact in (Compile, assembly), assembly)
+  ) settings (
       libraryDependencies ++= Seq(
         "org.brianmckenna" %% "wartremover" % "0.7",
         "net.databinder.dispatch" %% "dispatch-core" % "0.11.0",
